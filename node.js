8b4bafd0eca7d51ab10c6c2f644050d23e3e3db7
AVNSKS <anumulasiva2006@gmail.com>
2025-01-23 12:29:57 +0530

    node chnages

--- node.js
+++ node.js
@@ -1,24 +1,19 @@
-// Import the HTTP module
 const http = require('http');
 
-// Define the server
 const server = http.createServer((req, res) => {
-    // Set the response header
     res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // Send a response based on the request URL
     if (req.url === '/') {
-        res.end('Welcome to the Homepage!');
+        res.end('Hello! You are on the Home page.');
     } else if (req.url === '/about') {
-        res.end('This is the About page.');
+        res.end('Welcome to the About page!');
     } else {
         res.writeHead(404);
-        res.end('404 Not Found');
+        res.end('Oops! Page not found.');
     }
 });
 
-// Start the server and listen on port 3000
 const PORT = 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
